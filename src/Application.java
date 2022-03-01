2b3bbdb0e58d11d23f01f7a78391c532ed427ffc
Dhrushit Raval <dr9703@rit.edu>
2022-03-01 16:41:53 -0500

    adding setup for blinn phong

--- src/Application.java
+++ src/Application.java
@@ -8,7 +8,8 @@ public class Application {
         // Setups.setup3(this);
         // Setups.setup4(this);
         // Setups.setup5(this); // two light sources
-        Setups.setup6(this); // supersampling turned on
+        // Setups.setup6(this); // supersampling turned on
+        Setups.setup7(this); // phong blinn
     }
 
     private World world;
