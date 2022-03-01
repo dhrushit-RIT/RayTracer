d5c28fb62a4acab4a1a80ffafa76ff35bc103b00
Dhrushit Raval <dr9703@rit.edu>
2022-03-01 16:10:01 -0500

    adding description

--- src/Application.java
+++ src/Application.java
@@ -2,9 +2,9 @@ public class Application {
 
     public Application() {
         this.world = new World();
-        // Setups.setup0(this);
-        // Setups.setup1(this);
-        // Setups.setup2(this);
+        // Setups.setup0(this); // should not see anything
+        // Setups.setup1(this); // sphere at the center
+        // Setups.setup2(this); // // should not see anything
         // Setups.setup3(this);
         // Setups.setup4(this);
         Setups.setup5(this);
