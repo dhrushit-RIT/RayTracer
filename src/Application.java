47c90217f644297d9ef4508c94bfb1d93c1f936c
Dhrushit Raval <dr9703@rit.edu>
2022-03-01 14:30:32 -0500

    adding more setups

--- src/Application.java
+++ src/Application.java
@@ -3,8 +3,11 @@ public class Application {
     public Application() {
         this.world = new World();
         // Setups.setup0(this);
-        Setups.setup1(this);
+        // Setups.setup1(this);
         // Setups.setup2(this);
+        // Setups.setup3(this);
+        // Setups.setup4(this);
+        Setups.setup5(this);
     }
 
     private World world;
