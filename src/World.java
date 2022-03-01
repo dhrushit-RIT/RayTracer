7f8c8322019ae5be6158564c7ae7b8786be5395d
Dhrushit Raval <dr9703@rit.edu>
2022-03-01 10:40:55 -0500

    adding function to allow getting pixel irradiance

--- src/World.java
+++ src/World.java
@@ -30,13 +30,46 @@ public class World {
         this.camera.setWorld(this);
     }
 
+    public MyColor getPixelIrradiance(Ray ray) {
+        MyColor finalColor = new MyColor(128, 128, 128);
+        // MyColor finalColor = new MyColor(0, 0, 0);
+
+        boolean didGetIlluminated = false;
+        IntersectionDetails entityIntersectionDetails = this.checkIntersection(ray);
+        if (entityIntersectionDetails == null || entityIntersectionDetails.entity == null
+                || entityIntersectionDetails.intersectionPoint == null) {
+            return Camera.DEFAULT_COLOR;
+        }
+        for (Light light : lightSources) {
+            Ray shadowRay = new Ray(entityIntersectionDetails.intersectionPoint,
+                    // TODO: create a to Entity space and to World space in util and use that during
+                    // subtraction of points
+                    Util.subtract(Camera.toCameraSpace(light.position), entityIntersectionDetails.intersectionPoint));
+            IntersectionDetails intersectingEntity = checkIntersection(shadowRay);
+            if (intersectingEntity != null || (intersectingEntity != null && intersectingEntity.entity != null)) {
+                finalColor = new MyColor(0, 0, 0);
+            } else {
+                didGetIlluminated = true;
+                finalColor = entityIntersectionDetails.entity.getPixelIrradiance(
+                        light,
+                        camera,
+                        entityIntersectionDetails.intersectionPoint,
+                        entityIntersectionDetails.normalAtIntersection);
+            }
+
+        }
+
+        return finalColor;
+
+    }
+
     public IntersectionDetails checkIntersection(Ray cRay) {
         Entity nearestEntity = null;
         double nearestDistance = 100000000;
         IntersectionDetails bestIntersection = new IntersectionDetails(100000000);
         for (Entity entity : worldObjects) {
             IntersectionDetails intersection = entity.intersect(cRay);
-            if (intersection.distance > 0) {
+            if (intersection.distance > 0.001) {
                 if (intersection.distance < nearestDistance) {
                     nearestEntity = entity;
                     nearestDistance = intersection.distance;
