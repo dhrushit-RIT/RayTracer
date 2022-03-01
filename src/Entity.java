31736e6cd3f0948d1431c28bc3173e787004da30
Dhrushit Raval <dr9703@rit.edu>
2022-03-01 10:39:23 -0500

    adding BSDF and util functions

--- src/Entity.java
+++ src/Entity.java
@@ -6,6 +6,11 @@ public abstract class Entity {
     protected MyColor diffusedColor;
     protected Point position;
 
+    protected double ka = 0.1;
+    protected double kd = 0.4;
+    protected double ks = 0.5;
+    protected double ke = 180;
+
     public Entity(MyColor baseColor, Point position) {
         this.setBaseColor(baseColor);
 
@@ -20,6 +25,45 @@ public abstract class Entity {
         } else {
             this.baseColor = baseColor;
         }
+    }
+
+    public MyColor applyBSDF(Light light, Camera camera, Point intersecPoint, Vector normal) {
+        Vector lightDir = Util.subtract(Camera.toCameraSpace(light.position), intersecPoint);
+        lightDir.normalize();
+        double ambientFactor = ka * light.irradiance;
+        double diffuseFactor = kd * light.irradiance * Util.dot(lightDir, normal);
+
+        normal.normalize();
+
+        Vector reflectVector = Util.reflect(lightDir, normal, intersecPoint);
+        Vector view = Util.subtract(camera.getcPosition(), intersecPoint);
+        view.normalize();
+        double reflectDotView = Math.max(0.0, Util.dot(reflectVector, view));
+        double specularFactor = ks * light.irradiance * Math.pow(reflectDotView, ke);
+
+        MyColor ambient = Util.multColor(ambientFactor, baseColor);
+        MyColor diffuse = Util.multColor(diffuseFactor, diffusedColor);
+        MyColor specular = Util.multColor(specularFactor, specularColor);
+
+        MyColor finalColor = Util.addColor(ambient, diffuse, specular);
+        return finalColor;
+    }
+
+    public MyColor getPixelIrradiance(Light light, Camera camera, Point intersection, Vector normal) {
+        return this.applyBSDF(light, camera, intersection, normal);
+    }
+
+    public void setCoeffs(double ka, double kd, double ks, double ke) {
+        this.ka = ka;
+        this.kd = kd;
+        this.ks = ks;
+        this.ke = ke;
+    }
+
+    public void setColors(MyColor basecColor, MyColor speColor, MyColor diffuseColor) {
+        this.baseColor = basecColor;
+        this.specularColor = speColor;
+    this.diffusedColor = diffuseColor;
     }
 
     public abstract IntersectionDetails intersect(Ray ray);
