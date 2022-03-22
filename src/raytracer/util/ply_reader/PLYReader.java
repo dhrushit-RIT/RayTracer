b3de125c5cc6b0170bed338056fdce03034b152d
Dhrushit Raval <dr9703@rit.edu>
2022-03-22 12:59:28 -0400

    reader for ply files

---
+++ src/raytracer/util/ply_reader/PLYReader.java
@@ -0,0 +1,60 @@
+package raytracer.util.ply_reader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import raytracer.Point;
+import raytracer.Triangle;
+
+public class PLYReader {
+    public static ArrayList<Triangle> loadModel(String filePath) {
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(filePath))) {
+            ArrayList<Point> vList = new ArrayList<>();
+            ArrayList<Triangle> fList = new ArrayList<>();
+            int numVertices = -1;
+            int numFaces = -1;
+            boolean actualContent = false;
+            String line = br.readLine();
+            while (line != null) {
+                if (line.contains("end_header")) {
+                    actualContent = true;
+                } else if (line.contains("element vertex")) {
+                    numVertices = Integer.parseInt(line.split("\\s+")[2]);
+                } else if (line.contains("element face")) {
+                    numFaces = Integer.parseInt(line.split("\\s+")[2]);
+                } else if (actualContent) {
+                    String[] parts = line.split("\\s+");
+                    if (numVertices > 0) {
+                        double x = Double.parseDouble(parts[0]);
+                        double y = Double.parseDouble(parts[1]);
+                        double z = Double.parseDouble(parts[2]);
+                        vList.add(new Point(x, y, z, Point.Space.WORLD));
+                        numVertices--;
+                    }else
+
+                    if (numFaces > 0) {
+                        int numVerts = Integer.parseInt(parts[0]);
+                        int firstVert = Integer.parseInt(parts[0]);
+                        int secondVert = Integer.parseInt(parts[1]);
+                        int thirdVert = Integer.parseInt(parts[2]);
+                        fList.add(new Triangle(null, vList.get(firstVert), new Point[] {
+                                vList.get(firstVert),
+                                vList.get(secondVert),
+                                vList.get(thirdVert)
+                        }));
+                        numFaces--;
+                    }
+                }
+                line = br.readLine();
+            }
+            return fList;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
