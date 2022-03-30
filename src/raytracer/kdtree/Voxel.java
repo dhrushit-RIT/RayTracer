61f8d55efb530cb684333e6cbae7de6dd7500510
Dhrushit Raval <dr9703@rit.edu>
2022-03-30 19:37:11 -0400

    implemented voxel

--- src/raytracer/kdtree/Voxel.java
+++ src/raytracer/kdtree/Voxel.java
@@ -3,5 +3,17 @@ package raytracer.kdtree;
 import raytracer.BoundingBox;
 
 public class Voxel extends BoundingBox {
-    
+
+    public static int count = 0;
+    public static int leaf = 0;
+
+    public Voxel(double xMin, double xMax, double yMin, double yMax, double zMin, double zMax) {
+        super(xMin, xMax, yMin, yMax, zMin, zMax);
+        Voxel.count += 1;
+    }
+
+    public String toString() {
+        return "x: " + xMin + " - " + xMax + "\ny: " + yMin + " - " + yMax + "\nz: " + zMin + " - " + zMax;
+    }
+
 }
