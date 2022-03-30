313747f80eb68a4cd419e4ad2655ae7b1dacd9b6
Dhrushit Raval <dr9703@rit.edu>
2022-03-30 19:37:52 -0400

    intersect method

--- src/raytracer/kdtree/AAPlane.java
+++ src/raytracer/kdtree/AAPlane.java
@@ -1,17 +1,18 @@
 package raytracer.kdtree;
 
 import raytracer.Point;
+import raytracer.Ray;
 
 public class AAPlane {
 
-    enum Alignment {
+    public enum Alignment {
         XY,
         YZ,
         ZX
     }
 
-    Alignment alignment;
-    Point pointOnPlane;
+    public Alignment alignment;
+    public Point pointOnPlane;
 
     public AAPlane() {
         this.alignment = Alignment.XY;
@@ -28,8 +29,47 @@ public class AAPlane {
         this.pointOnPlane = new Point(pointOnPlane);
     }
 
-    public void setPointOnPlane(Point pointOnPlane){
+    public void setPointOnPlane(Point pointOnPlane) {
         this.pointOnPlane = pointOnPlane;
+    }
+
+    public String toString() {
+        return "AAP " + alignment + " | " + pointOnPlane;
+    }
+
+    public Point intersect(Ray ray) {
+        // Ax + By + Cz + F = 0
+        // XY : Cz + F = 0
+        // YZ : Ax + F = 0
+        // ZX : By + F = 0
+        // w = -1 * (Axo + Byo + Czo + F) / (Adx + Bdy + Cdz)
+        double A = 0;
+        double B = 0;
+        double C = 0;
+        double F = 0;
+
+        switch (alignment) {
+            case XY:
+                F = pointOnPlane.z;
+                break;
+            case YZ:
+                F = pointOnPlane.x;
+                break;
+            case ZX:
+                F = pointOnPlane.y;
+                break;
+        }
+
+        double w = -1 * (A * ray.origin.x + B * ray.origin.y + C * ray.origin.z + F)
+                / (A * ray.direction.x + B * ray.direction.y + C * ray.direction.z);
+
+        // if(w > 0) {
+        return new Point(ray.origin.x + ray.direction.x * w, ray.origin.y + ray.direction.y * w,
+                ray.origin.z + ray.direction.z * w, Point.Space.CAMERA);
+        // } else {
+        // return null;
+        // }
+
     }
 
 }
