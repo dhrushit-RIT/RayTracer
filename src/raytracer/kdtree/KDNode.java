856ab8a3eb8c9643c9f50f54fb2111f509028299
Dhrushit Raval <dr9703@rit.edu>
2022-03-30 19:37:38 -0400

    count num voxels created

--- src/raytracer/kdtree/KDNode.java
+++ src/raytracer/kdtree/KDNode.java
@@ -5,11 +5,18 @@ import java.util.ArrayList;
 import raytracer.Entity;
 
 public class KDNode {
+    AAPlane partition;
+    private KDNode left;
+    private KDNode right;
 
     public KDNode(AAPlane p, KDNode leftNode, KDNode rightNode) {
+        this.partition = p;
+        this.left = leftNode;
+        this.right = rightNode;
     }
 
     public KDNode(ArrayList<Entity> l) {
+        Voxel.leaf += 1;
     }
-    
+
 }
