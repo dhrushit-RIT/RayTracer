dff4a21e17e9958be9244dc8d5aa3899ea693cd5
Dhrushit Raval <dr9703@rit.edu>
2022-03-30 19:38:15 -0400

    compute world voxel

--- src/raytracer/World.java
+++ src/raytracer/World.java
@@ -1,5 +1,10 @@
 package raytracer;
+
 import java.util.ArrayList;
+
+import raytracer.kdtree.AAPlane;
+import raytracer.kdtree.KDTree;
+import raytracer.kdtree.Voxel;
 
 public class World {
     public static double EPSILON = 0.01;
@@ -12,10 +17,34 @@ public class World {
     ArrayList<Light> lightSources;
     private int superSampleFactor;
 
+    private Voxel worldVoxel;
+
+    private Object kdRoot;
+
     public World() {
         this.worldObjects = new ArrayList<>();
         this.lightSources = new ArrayList<>();
         this.superSampleFactor = 1;
+    }
+
+    private Voxel computeWorldVoxel() {
+        double xMin = Double.MAX_VALUE;
+        double yMin = Double.MAX_VALUE;
+        double zMin = Double.MAX_VALUE;
+        double xMax = -Double.MAX_VALUE;
+        double yMax = -Double.MAX_VALUE;
+        double zMax = -Double.MAX_VALUE;
+
+        for (Entity entity : this.worldObjects) {
+            xMin = Math.min(xMin, entity.boundingBox.xMin);
+            yMin = Math.min(yMin, entity.boundingBox.yMin);
+            zMin = Math.min(zMin, entity.boundingBox.zMin);
+            xMax = Math.max(xMax, entity.boundingBox.xMax);
+            yMax = Math.max(yMax, entity.boundingBox.yMax);
+            zMax = Math.max(zMax, entity.boundingBox.zMax);
+        }
+
+        return new Voxel(xMin, xMax, yMin, yMax, zMin, zMax);
     }
 
     public void setBSDFTechnique(Entity.BSDFTechnique technique) {
@@ -35,6 +64,9 @@ public class World {
     }
 
     public void simulate() {
+        this.worldVoxel = this.computeWorldVoxel();
+        this.kdRoot = KDTree.getNode(this.worldObjects, this.worldVoxel, AAPlane.Alignment.XY);
+        System.out.println("total voxels : " + Voxel.count + " leaf : " + Voxel.leaf);
         this.camera.takeASnap(this.superSampleFactor);
         this.camera.applyToneMapping();
         // this.camera.denormalizeColors(); // use this as separate pass later
