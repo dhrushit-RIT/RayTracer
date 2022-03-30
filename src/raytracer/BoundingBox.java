56ec7e57ba65ba3c57d9ad529530a06970bab430
Dhrushit Raval <dr9703@rit.edu>
2022-03-30 19:39:06 -0400

    intersection definition

--- src/raytracer/BoundingBox.java
+++ src/raytracer/BoundingBox.java
@@ -1,6 +1,5 @@
 package raytracer;
 
-
 public class BoundingBox {
 
     public double xMax;
@@ -10,15 +9,27 @@ public class BoundingBox {
     public double yMin;
     public double zMin;
 
+    public BoundingBox(double xMin, double xMax, double yMin, double yMax, double zMin, double zMax) {
+        this.xMax = xMax;
+        this.yMax = yMax;
+        this.zMax = zMax;
+        this.xMin = xMin;
+        this.yMin = yMin;
+        this.zMin = zMin;
+    }
+
     public boolean intersect(BoundingBox other) {
         boolean xNotIntersecting = this.xMax < other.xMin || other.xMax < this.xMin;
-        if(xNotIntersecting) return false;
-        
+        if (xNotIntersecting)
+            return false;
+
         boolean yNotIntersecting = this.yMax < other.yMin || other.yMax < this.yMin;
-        if(yNotIntersecting) return false;
+        if (yNotIntersecting)
+            return false;
 
         boolean zNotIntersecting = this.zMax < other.zMin || other.zMax < this.zMin;
-        if(zNotIntersecting) return false;
+        if (zNotIntersecting)
+            return false;
 
         return true;
     }
