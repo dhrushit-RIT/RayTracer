5b10e2c4250e1567165f752a52c918cd80d92dcd
Dhrushit Raval <dr9703@rit.edu>
2022-03-30 21:47:00 -0400

    terminal condition store voxel in kdtree

--- src/raytracer/kdtree/KDNode.java
+++ src/raytracer/kdtree/KDNode.java
@@ -5,18 +5,27 @@ import java.util.ArrayList;
 import raytracer.Entity;
 
 public class KDNode {
-    AAPlane partition;
-    private KDNode left;
-    private KDNode right;
+    public KDNode left;
+    public KDNode right;
+    public Voxel voxel;
+    public AAPlane partition;
+    public ArrayList<Entity> entities;
 
-    public KDNode(AAPlane p, KDNode leftNode, KDNode rightNode) {
+    public KDNode(AAPlane p, KDNode leftNode, KDNode rightNode, Voxel voxel) {
         this.partition = p;
         this.left = leftNode;
         this.right = rightNode;
+        this.voxel = voxel;
     }
 
-    public KDNode(ArrayList<Entity> l) {
+    public KDNode(ArrayList<Entity> l, Voxel voxel) {
         Voxel.leaf += 1;
+        this.entities = l;
+        this.voxel = voxel;
+    }
+
+    public boolean isTerminal() {
+        return this.left == null && this.right == null;
     }
 
 }
