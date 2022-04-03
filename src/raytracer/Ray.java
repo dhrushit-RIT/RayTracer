8213944cec298d0b96731f408e6f9a7fe84d7247
Dhrushit Raval <dr9703@rit.edu>
2022-04-03 17:59:02 -0400

    copy for ray and entity of origin

--- src/raytracer/Ray.java
+++ src/raytracer/Ray.java
@@ -4,10 +4,26 @@ public class Ray {
     public Point origin;
     public Vector direction;
 
+    public Entity originEntity;
+
     public Ray(Point origin, Vector direction) {
         this.origin = origin;
         this.direction = direction;
         this.direction.normalize();
+        this.originEntity = null;
+    }
+
+    public Ray(Ray other) {
+        this.origin = new Point(other.origin);
+        this.direction = new Vector(other.direction);
+        this.originEntity = other.originEntity;
+    }
+
+    public Ray(Point origin, Vector direction, Entity originEntity) {
+        this.origin = origin;
+        this.direction = direction;
+        this.direction.normalize();
+        this.originEntity = originEntity;
     }
 
     public Vector getDirection() {
