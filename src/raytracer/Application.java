1d769cf2544608b70eb06bb04357d8b1c4dc0561
Dhrushit Raval <dr9703@rit.edu>
2022-04-11 22:04:51 -0400

    use tone compressors

--- src/raytracer/Application.java
+++ src/raytracer/Application.java
@@ -1,13 +1,33 @@
 package raytracer;
 
+import raytracer.ToneMapping.Reinhard;
+import raytracer.ToneMapping.ToneCompressor;
+import raytracer.ToneMapping.Ward;
+
 public class Application {
+
+    public ToneCompressor toneCompressor;
+    public static ToneCompressor.Type compressorType;
 
     public Application() {
         this.world = new World();
+
         // Setups.DefaultSetups.setup7(this);
         // Setups.Refraction.setup0(this);
-        // Setups.AshikiminShirley.setup0(this);
-        Setups.LightFallout.setup0(this);
+        // Setups.AshikhiminShirley.setup0(this);
+        // Setups.LightFallout.setup0(this);
+        Setups.ToneMapping.setup2(this);
+    }
+
+    public ToneCompressor getToneCompressor() {
+        switch(compressorType) {
+            case WARD:
+                return new Ward();
+            case REINHARD:
+                return new Reinhard();
+            default:
+                return new Ward();
+        }
     }
 
     private World world;
